f70c5397a79f2b2ecdd6f0a2e6cb7ae22c170e2a
MD.Jahidul Islam <mdjahidulislamsarker@gmail.com>
2025-08-28 21:52:33 +0600

    Create SubarraySumEqualsK_PrefixSum.cpp

---
+++ Hashing/SubarraySumEqualsK_PrefixSum.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int subarraySum(vector<int>& arr, int k) {
+        int n=arr.size();
+        int count=0;
+        vector<int>prefixSum(n,0);
+        prefixSum[0]=arr[0];
+
+        for(int i=1;i<n;i++){
+            prefixSum[i]=prefixSum[i-1]+arr[i];
+        }
+        unordered_map<int,int>m;
+        for(int j=0;j<n;j++){
+            if(prefixSum[j]==k) count++;
+
+            int val=prefixSum[j]-k;
+
+            if(m.find(val) != m.end()){
+                count+=m[val];
+            }
+            if(m.find(prefixSum[j]) == m.end()){
+              m[prefixSum[j]]=0;
+            }
+             m[prefixSum[j]]++;
+        }
+        return count;
+    }
+};
+
+int main() {
+    Solution sol;
+    int n, k;
+    cin >> n;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++) cin >> arr[i];
+    cin >> k;
+
+    cout << sol.subarraySum(arr, k) << endl;
+
+    return 0;
+}
