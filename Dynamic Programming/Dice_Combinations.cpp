d6c14f60259ac3a031e0de2b81332a7807e0e052
MD.Jahidul Islam <mdjahidulislamsarker@gmail.com>
2026-07-15 01:09:28 +0600

    Add files via upload

---
+++ Dynamic Programming/Dice_Combinations.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+const int MOD = 1e9 + 7;
+
+#define ll long long
+
+int diceComb(ll n){
+    vector<ll>dp(n+1,0);
+
+    dp[0]=1;
+
+    for(ll sum=1;sum<=n;sum++){
+        for(ll dice=1;dice<=6;dice++){
+            if(sum-dice>=0){
+                dp[sum]=(dp[sum] + dp[sum-dice]) % MOD;
+            }
+        }
+    }
+    return dp[n];
+
+}
+int main() {
+    ll n;
+    cin>>n;
+
+    cout<<diceComb(n)<<endl;
+    return 0;
+}
